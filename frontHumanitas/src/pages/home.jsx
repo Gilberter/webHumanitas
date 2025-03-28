70b6b76685ee9fba5249da300c95589ae7a497db
juanleon04 <jdavidleon04@gmail.com>
2025-03-28 06:31:03 -0500

    Creación de inicio y login

---
+++ frontHumanitas/src/pages/home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from '../componentes/header.jsx';
+
+const Home = () => {
+  return (
+    <>
+        <Header />
+        <div>
+            <h1>Inicio</h1>
+            <p>Esta es el login.</p>
+        </div>
+    </>
+  );
+};
+
+export default Home;
---
+++ frontHumanitas/src/pages/login.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./login.css";
+
+const Login = () => {
+  return (
+    <>
+    <div className="login-container">
+        <div className="iniciarSesion">
+            <h1>Iniciar Sesión</h1>
+            <p>Esta es el inicio de sesión.</p>
+        </div>
+        
+        <div className="registrarse">
+            <h1>Registrarse</h1>
+            <p>Esta es el registro.</p>
+        </div>
+    </div>
+    </>
+  );
+};
+
+export default Login;
